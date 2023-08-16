9a39d2c6662045696a2205d4b566c7a43aab3ec2
Martín Pereira <veridicotp22@gmail.com>
2023-08-16 19:49:33 -0300

    Añadimos guardado de usuario

--- js/index.js
+++ js/index.js
@@ -11,4 +11,13 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
+    let usuario = localStorage.getItem('nombre');
+    if (usuario=="" || usuario==null){
+        location.href='login.html';
+    }else{
+        document.getElementById('nombre').innerHTML= usuario;
+    }
+
 });
+
