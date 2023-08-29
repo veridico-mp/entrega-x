aa64b012633ca62642fca03bfe80223c633c16c4
Martín Pereira <veridicotp22@gmail.com>
2023-08-29 19:41:57 -0300

    Aplique filtro segun precio y clean

--- js/products.js
+++ js/products.js
@@ -26,8 +26,8 @@
     
     
           
-    //Toma un array de productos y crea un div a medida que itera por cada elemento, dentro coloca todas sus características   
-          function uploadProducts(dataArray) {
+        //Toma un array de productos y crea un div a medida que itera por cada elemento, dentro coloca todas sus características   
+        function uploadProducts(dataArray) {
             const productsList = document.getElementById("products-list");
           
             for (const item of dataArray) {
@@ -38,8 +38,9 @@
               productDiv.innerHTML = `
                   <img src="${item.image}">
                   <div class="description-container">
-                      <h2>${item.name} ${item.currency} ${item.cost}</h2>
+                      <h2>${item.name}</h2>
                       <p>${item.description}</p>
+                      <p class="price">${item.currency} ${item.cost}</p>
                   </div>
                   <div class="sold-count">
                       <span>${item.soldCount} vendidos</span>
@@ -47,4 +48,47 @@
               `;
               productsList.appendChild(productDiv);
             }
-          }
+            
+            /*Aca esta el filtro de productos segun precio */
+            const filterBtn = document.getElementById('rangeFilterCount');
+            filterBtn.addEventListener('click', function(){
+                let min = document.getElementById('rangeFilterCountMin').value;
+                let max = document.getElementById('rangeFilterCountMax').value;
+                //Aca nos aseguramos de que se contemplen los casos en que el usuario deja un campo vacio
+                if (min === "" && max === ""){//Si ambos "minimo y maximo" estan vacios no hace nada mas q enviar un mensaje a consola
+                    console.log("Los campos estan vacíos")//Esto se puede sustituír por una alerta visual para el usuario
+                }else if (min >=0 && max === ""){
+                    productsList.innerHTML= "";
+                    let filtrado = dataArray.filter(item => item.cost >= min);
+                    uploadProducts(filtrado);
+                }else if (min === "" && max >= 0){
+                    productsList.innerHTML= "";
+                    let filtrado = dataArray.filter(item => item.cost <= max);
+                    uploadProducts(filtrado);
+                }else {
+                    productsList.innerHTML= "";
+                    let filtrado = dataArray.filter(item => item.cost >= min && item.cost <= max);
+                    uploadProducts(filtrado);
+                }
+            })
+            
+
+        }
+        /*Aca se limpian los filtros y se vuelve a cargar los productos en su totalidad */
+            const cleanFilter = document.getElementById('clearRangeFilter');
+            cleanFilter.addEventListener('click', function(){
+                const productsList = document.getElementById("products-list");
+                productsList.innerHTML= "";
+                
+                fetchProductData(URL_CATEGORIES)
+                .then(data => {
+                    uploadProducts(data.products);
+                })
+                .catch(error => {
+                    const errorMessage = document.createElement("p");
+                    errorMessage.textContent = "Hubo un error al cargar los productos.";
+                    document.body.appendChild(errorMessage);
+                });
+
+            })
+
