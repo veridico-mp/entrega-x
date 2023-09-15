5d30a9b9fd88ce04605e8ba414df547262c6ee46
Nicolás Ortega <nicoortega39@gmail.com>
2023-09-15 12:31:39 -0300

    Slideshow basico
    Agregado slideshow y diseño basico

--- js/product-info.js
+++ js/product-info.js
@@ -12,8 +12,10 @@ function fetchData(url){//Esta es para mostrar imagenes
     .then(data => {
         console.log(data);
         showProductGalery(data);
+        showMainInfo(data)
         showProductDescription(data);
         showRelatedProducts(data);
+        
     })
     .catch(function(error) {
         console.log(error);
@@ -32,28 +34,83 @@ function fetchCom(url){//Esta es para los comentarios
 }
 
 function showProductGalery(data){//Muestra galeria de imagenes.
-    let cont = document.getElementById('contenedor');
-    cont.innerHTML+= `
-    <h1>${data.name}</h1>
-    <div class='imgList' id='imList'></div>
-    <div class='showImg' id='showImg'></div>
-    <div class='prodInfo'></div>
-    `;
+
     showImgList(data);
+    showSlides(1);
+    
 }
+
+
+
 function showImgList(data){//Agrega imagenes a la lista.
-    let imgList= document.getElementById('imList');
+    let imgList= document.getElementById('expImg');
+    let imgRow = document.getElementById('row')
+    let numImg = 1;
     for(let one of data.images){
-        imgList.innerHTML+=`<img src='${one}' onclick='expose("${one}")'>`;
+
+        imgList.innerHTML+=`<div class="mySlides">
+        <div class="numbertext">${numImg} / ${data.images.length}</div>
+        <img src='${one}' onclick='expose("${one}") style="width:100%'>
+      </div>`;
+
+
+      imgRow.innerHTML+= `<div class="column">
+      <img class="demo cursor" src="${one}"  onclick="currentSlide(${numImg})" style="width:100%;";" >
+        </div>`;
+
+
+      numImg++;
     }
 }
-function expose(img){//Muestra las imagenes ampliadas.
-    let screen= document.getElementById('showImg');
-    screen.innerHTML = `<img src=${img}>`;
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Controles
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
+
+function showMainInfo(data){
+  let des = document.getElementById('mainInfo');
+  let prodName = document.getElementById('prodName');
+  let prodCost = document.getElementById('prodCost');
+  prodName.innerHTML += `${data.name}`
+  prodCost.innerHTML += `${data.currency}:  ${data.cost}`
+ 
+}
+
+
+
+
+
+
 function showProductDescription(data){
     let des = document.getElementById('descripcion');
-    des.innerHTML += `<h2>descripción:</h2> <br> ${data.description}<br> valor: ${data.currency} ${data.cost}<br>Stock: ${data.soldCount}`;
+
+    des.innerHTML += `<h2>Descripción:</h2> <br> ${data.description}<br>`;
         console.log(des);    
 }
 
