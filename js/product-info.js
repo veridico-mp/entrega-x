19da3d57044aeeea8ce59678d8ab1af5e989f36e
caroantunez <caro.antunez2007@gmail.com>
2023-09-15 12:36:26 -0300

    Agrega funcionalidad de comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -2,70 +2,93 @@
 var prodID = localStorage.getItem("prodID");
 let URL_prod = `https://japceibal.github.io/emercado-api/products/${prodID}.json`;
 let URL_com = `https://japceibal.github.io/emercado-api/products_comments/${prodID}.json`;
+
 // Hacer la solicitud fetch para obtener la información del producto
 fetchData(URL_prod);
-//fetchCom(URL_com);
-//Funcion para cargar contenidos.
-function fetchData(url){//Esta es para mostrar imagenes
-    fetch(url)
-    .then(response=> response.json())
-    .then(data => {
-        console.log(data);
-        showProductGalery(data);
-        showProductDescription(data);
-        showRelatedProducts(data);
+
+// Función para cargar contenidos.
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      showProductGallery(data);
+      showProductDescription(data);
+      showRelatedProducts(data);
     })
-    .catch(function(error) {
-        console.log(error);
-    });
-}
-function fetchCom(url){//Esta es para los comentarios
-    fetch(url)
-    .then(response=> response.json())
-    .then(data => {
-        console.log(data);
-        //showProductInfo(data);
-    })
-    .catch(function(error) {
-        console.log(error);
+    .catch(function (error) {
+      console.log(error);
     });
 }
 
-function showProductGalery(data){//Muestra galeria de imagenes.
-    let cont = document.getElementById('contenedor');
-    cont.innerHTML+= `
+// Función para cargar comentarios.
+function fetchComments(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      showProductComments(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function showProductGallery(data) {
+  let galleryContainer = document.getElementById("gallery");
+  galleryContainer.innerHTML = ""; // Limpia el contenido anterior si lo hubiera
+
+  for (let image of data.images) {
+    let imageElement = document.createElement("img");
+    imageElement.src = image;
+    galleryContainer.appendChild(imageElement);
+  }
+}
+
+function showProductDescription(data) {
+  let descriptionContainer = document.getElementById("description");
+  descriptionContainer.innerHTML = `
     <h1>${data.name}</h1>
-    <div class='imgList' id='imList'></div>
-    <div class='showImg' id='showImg'></div>
-    <div class='prodInfo'></div>
-    `;
-    showImgList(data);
-}
-function showImgList(data){//Agrega imagenes a la lista.
-    let imgList= document.getElementById('imList');
-    for(let one of data.images){
-        imgList.innerHTML+=`<img src='${one}' onclick='expose("${one}")'>`;
-    }
-}
-function expose(img){//Muestra las imagenes ampliadas.
-    let screen= document.getElementById('showImg');
-    screen.innerHTML = `<img src=${img}>`;
-}
-function showProductDescription(data){
-    let des = document.getElementById('descripcion');
-    des.innerHTML += `<h2>descripción:</h2> <br> ${data.description}<br> valor: ${data.currency} ${data.cost}<br>Stock: ${data.soldCount}`;
-        console.log(des);    
+    <p>${data.description}</p>
+    <p>Precio: ${data.currency} ${data.cost}</p>
+    <p>Stock: ${data.soldCount}</p>
+  `;
 }
 
-function showRelatedProducts(data){ // Funcion que mostrara los productos relacionados
-    let relproduct= document.getElementById('prodRelacionados');
-    relproduct.innerHTML += `
-    <div class='container form-control' id='relprod'></div>`
-    showProductRelacionado(data);
-};
-function showProductRelacionado(data){ // Esta funcion obtendra los productos del array y luego se llamará dentro de showRelatedProducts
- let relprod = document.getElementById('relprod');
- for (let product of data.relatedProducts){
-    relprod.innerHTML += `<h2>Productos Relacionados:</h2> <br> <p>${product.name}</p> <img src=${product.image}>`;
- }
+function showRelatedProducts(data) {
+  let relatedProductsContainer = document.getElementById("related-products");
+  relatedProductsContainer.innerHTML = "<h2>Productos Relacionados:</h2>";
+
+  for (let relatedProduct of data.relatedProducts) {
+    let productElement = document.createElement("div");
+    productElement.classList.add("related-product");
+
+    productElement.innerHTML = `
+      <h3>${relatedProduct.name}</h3>
+      <img src="${relatedProduct.image}" alt="${relatedProduct.name}">
+    `;
+
+    relatedProductsContainer.appendChild(productElement);
+  }
 }
+
+function showProductComments(data) {
+  let commentsContainer = document.getElementById("comments");
+  commentsContainer.innerHTML = ""; // Limpia el contenido anterior si lo hubiera
+
+  for (let comment of data) {
+    let commentElement = document.createElement("div");
+    commentElement.classList.add("comment");
+
+    commentElement.innerHTML = `
+      <h3>Comentario:</h3>
+      <p>Puntuación: ${comment.score}</p>
+      <p>Usuario: ${comment.user}</p>
+      <p>Descripción: ${comment.description}</p>
+      <p>Fecha y hora: ${comment.dateTime}</p>
+    `;
+
+    commentsContainer.appendChild(commentElement);
+  }
+}
+
+// Llamamos a la función para cargar los comentarios
+fetchComments(URL_com);
