698d4ac41b6ccfa6dbf8644f9ddef1ea6b75073e
Martín Pereira <veridicotp22@gmail.com>
2023-09-15 03:23:57 -0300

    Modificaciones miercoles

--- js/product-info.js
+++ js/product-info.js
@@ -4,7 +4,6 @@ let URL_prod = `https://japceibal.github.io/emercado-api/products/${prodID}.json
 let URL_com = `https://japceibal.github.io/emercado-api/products_comments/${prodID}.json`;
 // Hacer la solicitud fetch para obtener la información del producto
 fetchData(URL_prod);
-//fetchCom(URL_com);
 //Funcion para cargar contenidos.
 function fetchData(url){//Esta es para mostrar imagenes
     fetch(url)
@@ -12,7 +11,7 @@ function fetchData(url){//Esta es para mostrar imagenes
     .then(data => {
         console.log(data);
         showProductGalery(data);
-        //showProductDescription(data);
+        showProductDescription(data);
     })
     .catch(function(error) {
         console.log(error);
@@ -23,7 +22,7 @@ function fetchCom(url){//Esta es para los comentarios
     .then(response=> response.json())
     .then(data => {
         console.log(data);
-        //showProductInfo(data);
+        showProductInfo(data);
     })
     .catch(function(error) {
         console.log(error);
@@ -36,7 +35,7 @@ function showProductGalery(data){//Muestra galeria de imagenes.
     <h1>${data.name}</h1>
     <div class='imgList' id='imList'></div>
     <div class='showImg' id='showImg'></div>
-    <div class='prodInfo'></div>
+    <div class='prodInfo' id='prodInfo'></div>
     `;
     showImgList(data);
 }
@@ -51,5 +50,11 @@ function expose(img){//Muestra las imagenes ampliadas.
     screen.innerHTML = `<img src=${img}>`;
 }
 function showProductDescription(data){
-    let cont = document.getElementById('contenedor');
+    let cont = document.getElementById('prodInfo');
+    cont.innerHTML+= `
+    <div class='descInfo'>
+    <p>${data.description}</p>
+    <p>Precio: ${data.currency} ${data.cost}</p>
+    </div>
+    `;
 }
