c754af9b4e1b2c8594124718a44b811a3dd7e83e
Joaquín Gil <joacogilp@gmail.com>
2023-09-16 15:55:07 -0300

    Se arregla el merge de comentarios y productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -5,6 +5,51 @@ let URL_com = `https://japceibal.github.io/emercado-api/products_comments/${prod
 
 // Hacer la solicitud fetch para obtener la información del producto
 fetchData(URL_prod);
+fetchComments(URL_com);
+
+// Obtener una referencia al formulario y al contenedor de comentarios
+const commentForm = document.getElementById('comment-form');
+const commentsContainer = document.getElementById('comments');
+
+// Agregar un evento de envío al formulario
+commentForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Evitar que la página se recargue al enviar el formulario
+
+  // Obtener los valores ingresados por el usuario
+  let username = localStorage.getItem('nombre');
+  let score = Number(document.getElementById('score').value);
+  let commentText = document.getElementById('comment').value;
+
+  if (score === -1) {
+    alert('Tu comentario debe ir acompañado de una puntuación.');
+    return;
+  }
+  // Validar que se haya ingresado un comentario
+  if (commentText.trim() === '') {
+    alert('Por favor, ingrese un comentario.');
+    return;
+  }
+
+  // Obtener la fecha y hora actual
+  const now = new Date();
+  const formattedDate = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+
+  // Crear un nuevo elemento de comentario y agregarlo al contenedor de comentarios
+  const newComment = document.createElement('div');
+  newComment.className = 'comment';
+  newComment.innerHTML = `
+    <p>Puntuación: ${score}</p>  
+    <p>${username}</p>
+    <p>Comentario: ${commentText}</p>
+    <p>Fecha y Hora: ${formattedDate}</p>
+    <hr>
+  `;
+
+  commentsContainer.appendChild(newComment);
+
+  // Limpiar el formulario después de enviar el comentario
+  commentForm.reset();
+});
 
 document.addEventListener('DOMContentLoaded', function () {
   //Nombre de usuario y boton desconectar
@@ -30,6 +75,7 @@ function fetchData(url) {
     .then(response => response.json())
     .then(data => {
       showProductGalery(data);
+      showMainInfo(data);
       showProductDescription(data);
       showRelatedProducts(data);
     })
@@ -43,8 +89,7 @@ function fetchComments(url) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      showProductInfo(data);
+      showProductComments(data);
     })
     .catch(function (error) {
       console.log(error);
@@ -133,71 +178,25 @@ function showProductRelacionado(data) {
   for (let product of data.relatedProducts) {
     relprod.innerHTML += ` <div class=containerRelProd> <div class="product-info"> <img src=${product.image}>  <p>${product.name}</p> </div></div>`;
   }
+}
 
-  function showProductComments(data) {
-    let commentsContainer = document.getElementById('comments');
-    commentsContainer.innerHTML = ''; // Limpia el contenido anterior si lo hubiera
+function showProductComments(data) {
+  let commentsContainer = document.querySelector('#comments');
+  commentsContainer.innerHTML = ''; // Limpia el contenido anterior si lo hubiera
 
-    for (let comment of data) {
-      let commentElement = document.createElement('div');
-      commentElement.classList.add('comment');
+  for (let comment of data) {
+    let commentElement = document.createElement('div');
+    commentElement.classList.add('comment');
 
-      commentElement.innerHTML = `
+    commentElement.innerHTML = `
       <p>Puntuación: ${comment.score}</p>
       <p>${comment.user}</p>
-      <p>Descripción: ${comment.description}</p>
+      <p>Comentario: ${comment.description}</p>
       <p>Fecha y hora: ${comment.dateTime}</p>
       <br> <hr>
       
     `;
 
-      commentsContainer.appendChild(commentElement);
-    }
+    commentsContainer.appendChild(commentElement);
   }
-
-  // Llamamos a la función para cargar los comentarios
-  fetchComments(URL_com);
-
-  // Obtener una referencia al formulario y al contenedor de comentarios
-  const commentForm = document.getElementById('comment-form');
-  const commentsContainer = document.getElementById('comments');
-
-  // Agregar un evento de envío al formulario
-  commentForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evitar que la página se recargue al enviar el formulario
-
-    // Obtener los valores ingresados por el usuario
-    let username = localStorage.getItem('nombre');
-    let score = Number(document.getElementById('score').value);
-    let commentText = document.getElementById('comment').value;
-
-    if (score === -1) {
-      alert('Tu comentario debe ir acompañado de una puntuación.');
-      return;
-    }
-    // Validar que se haya ingresado un comentario
-    if (commentText.trim() === '') {
-      alert('Por favor, ingrese un comentario.');
-      return;
-    }
-
-    // Obtener la fecha y hora actual
-    const now = new Date();
-    const formattedDate = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
-
-    // Crear un nuevo elemento de comentario y agregarlo al contenedor de comentarios
-    const newComment = document.createElement('div');
-    newComment.className = 'comment';
-    newComment.innerHTML = `
-    <p>Puntuación: ${score}</p>  
-    <p>${username}</p>
-    <p>Comentario: ${commentText}</p>
-    <p>Fecha y Hora: ${formattedDate}</p>
-  `;
-
-    commentsContainer.appendChild(newComment);
-
-    // Limpiar el formulario después de enviar el comentario
-    commentForm.reset();
-  });
 }
