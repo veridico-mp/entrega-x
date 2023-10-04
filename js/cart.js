ee6202c2528cc9b6d66c499bb03e5040baef022c
Martín Pereira <veridicotp22@gmail.com>
2023-10-04 14:47:19 -0300

    Version 2

--- js/cart.js
+++ js/cart.js
@@ -9,4 +9,34 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function showList(data) {}
+function showList(data) {
+    let list = document.getElementById("listaCarrito");
+    for(let one of products){
+        list.innerHTML += `
+        <div class="form-control col-md-6" id="cssList">
+            <ul>
+                <div class="form-control col-md-6">
+                    <li></li>
+                    <img src="${one.article.image}">
+                </div>
+                <div class="form-control col-md-6">
+                    <li>Nombre</li>
+                    <p>${one.article.name}</p>
+                </div>
+                <div class="form-control col-md-6">
+                    <li>Costo</li>
+                    <p>${one.article.currency}${one.article.unitCost}</p>
+                </div>
+                <div class="form-control col-md-6">
+                    <li>Cant.</li>
+                    <input type="number" id="units" min="1">
+                </div>
+                <div class="form-control col-md-6">
+                    <li>Subtotal</li>
+                    ${"una funcion"};//Aca va el subtotal
+                </div>
+            </ul>
+        </div>
+        `
+    }
+}
