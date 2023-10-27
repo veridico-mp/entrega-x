867a9b6f4a2f0a585741c5d1f486f86a538c7a03
Joaquín Gil <joacogilp@gmail.com>
2023-10-27 17:48:04 -0300

    Se modifican los subtotales en el carrito

--- js/cart.js
+++ js/cart.js
@@ -35,39 +35,48 @@ function showList(data) {
                 <div class="col text-center fnt-size px-1">${one.name}</div>
                 <div class="col text-center fnt-size px-0 py-0"><div class="row"><div class="col">${one.currency}</div><div class="col cost">${one.unitCost}</div></div></div>
                 <div class="col text-center fnt-size px-1 py-1"><input type="number" id="units" min="1" value="${one.count}" class="cantidadProd"></div>
-                <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${one.currency}</div><div class="col subTot">${calcularSubtotal(one.unitCost, 1)}</div></div></div>
+                <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${one.currency}</div><div class="col subTot">${one.unitCost * one.count}</div></div></div>
             </div>
         </div>
         `;
-    /*Se crea la clase "cost" que contiene el valor numerico del costo unitario.
-         Se crea la clase "subTot" en la cual se debe introducir el valor subtotal calculado*/
-
-    console.log(values[2].value);
   }
+  let cantidadInputs = document.querySelectorAll('.cantidadProd');
+  cantidadInputs.forEach(input => {
+    input.addEventListener('change', modificarSubtotal);
+  });
 }
 
 function showListFromStorage(data) {
   let list = document.getElementById('listaCarrito');
   for (let article of data) {
     list.innerHTML += `
-          <div class="form-control py-1" id="cssList">
-              <div class="row py-0">
-                  <div class="col text-center fnt-size px-1"><img src="${article.Imagen}" title="producto" class="imagenCart img-fluid float-start"></div>
-                  <div class="col text-center fnt-size px-1">${article.Nombre}</div>
-                  <div class="col text-center fnt-size px-0 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col cost">${article.CosteUnidad}</div></div></div>
-                  <div class="col text-center fnt-size px-1 py-1"><input type="number" onchange="" min="1" value="${article.Cantidad}" class="cantidadProd"></div>
-                  <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col subTot">${calcularSubtotal(
-      article.CosteUnidad,
-      article.Cantidad
-    )}</div></div></div>
-
-              </div>
-          </div>
-          `;
+      <div class="form-control py-1" id="cssList">
+        <div class="row py-0">
+          <div class="col text-center fnt-size px-1"><img src="${article.Imagen}" title="producto" class="imagenCart img-fluid float-start"></div>
+          <div class="col text-center fnt-size px-1">${article.Nombre}</div>
+          <div class="col text-center fnt-size px-0 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col cost">${article.CosteUnidad}</div></div></div>
+          <div class="col text-center fnt-size px-1 py-1"><input type="number" min="1" value="${article.Cantidad}" class="cantidadProd"></div>
+          <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col subTot">${article.Cantidad * article.CosteUnidad}</div></div></div>
+        </div>
+      </div>
+    `;
   }
+
+  // Agregar el evento change a todos los elementos con la clase "cantidadProd"
+  let cantidadInputs = document.querySelectorAll('.cantidadProd');
+  cantidadInputs.forEach(input => {
+    input.addEventListener('change', modificarSubtotal);
+  });
 }
 
-function calcularSubtotal(precioProducto, cantidadProducto) {
-  subtotal = precioProducto * cantidadProducto;
-  return subtotal;
+function modificarSubtotal() {
+  let cantidadInputs = document.querySelectorAll('.cantidadProd');
+  let preciosProducto = document.querySelectorAll('.cost');
+  let subtotales = document.querySelectorAll('.subTot');
+
+  for (let i = 0; i < cantidadInputs.length; i++) {
+    let cantidad = parseInt(cantidadInputs[i].value);
+    let precio = parseFloat(preciosProducto[i].textContent);
+    subtotales[i].textContent = cantidad * precio;
+  }
 }
