7b1f7c14e2802ccc4c29d40c11a322b0536b29a2
Martín Pereira <veridicotp22@gmail.com>
2023-10-29 23:25:48 -0300

    Arreglo subTot calculo

--- js/cart.js
+++ js/cart.js
@@ -24,7 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
   let values = document.getElementsByClassName('cantidadProd');
   let envio = document.getElementById('tipoEnvio'); //Este es el div que contiene los radio check para el tipo de envio.
 
-  //calcularCostos();
+  calcularCostos();
+  /*values.addEventListener('change', () => {
+    let costElements = document.querySelectorAll('.cost').innerHTML;
+    console.log(costElements);
+    // calcularSubtotal(precioProducto, cantidadProducto);
+  });*/
 });
 
 function showList(data) {
@@ -35,9 +40,9 @@ function showList(data) {
             <div class="row py-0">
                 <div class="col text-center fnt-size px-1"><img src="${one.image}" title="producto" class="imagenCart img-fluid float-start"></div>
                 <div class="col text-center fnt-size px-1">${one.name}</div>
-                <div class="col text-center fnt-size px-0 py-0"><div class="col"><div class="col">${one.currency}</div><div class="col cost">${one.unitCost}</div></div></div>
+                <div class="col text-center fnt-size px-0 py-0"><div class="row"><div class="col">${one.currency}</div><div class="col cost">${one.unitCost}</div></div></div>
                 <div class="col text-center fnt-size px-1 py-1"><input type="number" id="units" min="1" value="${one.count}" class="cantidadProd"></div>
-                <div class="col text-center fnt-size px-1 py-0"><div class="col"><div class="col">${one.currency}</div><div class="col subTot">${one.unitCost * one.count}</div></div></div>
+                <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${one.currency}</div><div class="col subTot">${one.unitCost * one.count}</div></div></div>
             </div>
         </div>
         `;
@@ -61,7 +66,7 @@ function showListFromStorage(data) {
         <div class="row py-0">
           <div class="col text-center fnt-size px-1"><img src="${article.Imagen}" title="producto" class="imagenCart img-fluid float-start"></div>
           <div class="col text-center fnt-size px-1">${article.Nombre}</div>
-          <div class="col text-center fnt-size px-0 py-0"><div class="col"><div class="col">${article.Divisa}</div><div class="col cost">${article.CosteUnidad}</div></div></div>
+          <div class="col text-center fnt-size px-0 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col cost">${article.CosteUnidad}</div></div></div>
           <div class="col text-center fnt-size px-1 py-1"><input type="number" min="1" value="${article.Cantidad}" class="cantidadProd"></div>
           <div class="col text-center fnt-size px-1 py-0"><div class="row"><div class="col">${article.Divisa}</div><div class="col subTot">${article.Cantidad * article.CosteUnidad}</div></div></div>
         </div>
@@ -99,7 +104,7 @@ function calcularCostos() {
 
   for (let i = 0; i < preciosProductos.length; i++) {
     costo = Number(preciosProductos[i].innerHTML);
-    costeDeProductosTotal = "";
+    costeDeProductosTotal = 0;
     costeDeProductosTotal += costo;
   }
 
@@ -292,22 +297,3 @@ BotónComprar.addEventListener('click', () => {
 function mostrarMensajeError(elemento, mensaje) {
   elemento.textContent = mensaje;
 }
-
-document.getElementById("BotóndeCompra").addEventListener("click", function() {
-  let tipoEnvioInputs = document.getElementsByName("envío");
-  let seleccionado = false;
-
-  for (let i = 0; i < tipoEnvioInputs.length; i++) {
-    if (tipoEnvioInputs[i].checked) {
-      seleccionado = true;
-      break;
-    }
-  }
-
-  if (!seleccionado) {
-    document.getElementById("errorMensaje").textContent = "Por favor, seleccione un tipo de envío.";
-    return false;
-  }
-
-  // Continuar con el proceso de finalizar compra si se ha seleccionado un tipo de envío.
-});
