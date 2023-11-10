286d14dc2ac47b02f685c97897c0e7457b048240
EmilianoMozzone <emilianomozzone@hotmail.com>
2023-11-09 21:26:57 -0300

    modificacion del formulario y añado foto con su script

--- js/my-profile.js
+++ js/my-profile.js
@@ -13,3 +13,24 @@ function toProfile() {
     location.href = 'login.html';
   }
 }
+
+// Script para cambiar imagen del perfil
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener el elemento de entrada de archivo y el elemento de imagen
+  const imagenInput = document.getElementById("imagen");
+  const perfilImagen = document.getElementById("perfilImagen");
+
+  // Escuchar cambios en el campo de entrada de archivo
+  imagenInput.addEventListener("change", function (event) {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      // Crear una URL del archivo seleccionado
+      const imageUrl = URL.createObjectURL(selectedFile);
+
+      // Actualizar la imagen del perfil con la imagen seleccionada
+      perfilImagen.src = imageUrl;
+    }
+  });
+});
