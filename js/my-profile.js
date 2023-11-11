52007e44ce968c1777ec275f0131af44fcb8a002
EmilianoMozzone <emilianomozzone@hotmail.com>
2023-11-10 22:15:09 -0300

    actualizacion de validaciones

--- js/my-profile.js
+++ js/my-profile.js
@@ -34,3 +34,63 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// Script para validaciones del perfil y guardar en el local Storage
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  let botonConfirmarCambios = document.getElementById('guardarCambios');
+  
+  let datosUsuario = {};
+
+  botonConfirmarCambios.addEventListener('click', () => {
+
+  let mensajeDeErrorNombre = document.getElementById('mensajeErrorNombre');
+  let mensajeDeErrorApellido = document.getElementById('mensajeErrorApellido');
+  let mensajeDeErrorTelefono = document.getElementById('mensajeErrorTelefono');
+  let alerta = document.getElementById('alerta');
+    
+    let firstName = document.getElementById('primerNombre').value;
+    let firstLastName = document.getElementById('primerApellido').value;
+    let secondName = document.getElementById('segundoNombre').value;
+    let secondLastName = document.getElementById('segundoApellido').value;
+    let correo = document.getElementById('email').value;
+    let phone = document.getElementById('telefono').value;
+
+    mensajeDeErrorNombre.textContent = " ";
+    mensajeDeErrorApellido.textContent = " ";
+    mensajeDeErrorTelefono.textContent = " ";
+
+    if (firstName === "") {
+      mensajeDeErrorNombre.textContent = 'Por favor, debe ingresar un nombre';
+    } else {
+      localStorage.setItem("name", firstName);
+    }
+
+    if (firstLastName === "") {
+      mensajeDeErrorApellido.textContent = 'Por favor, debe ingresar un apellido';
+    } else {
+      localStorage.setItem("lastName", firstLastName);
+    }
+
+    if (phone === "") {
+      mensajeDeErrorTelefono.textContent = 'Por favor, debe ingresar un teléfono de contacto';
+    } else {
+      localStorage.setItem("Telefono", phone);
+    }
+
+    if (secondName !== "" && secondLastName !== "" && correo !== "") {
+      datosUsuario = {
+        segundoNombre: secondName,
+        segundoApellido: secondLastName,
+        email: correo
+      };
+      localStorage.setItem('datosdelUsuario', JSON.stringify(datosUsuario));
+    };
+    
+    // Mostrar alerta de Bootstrap
+    console.log(alerta);
+    alerta.classList.add('show');
+
+  });
+});
